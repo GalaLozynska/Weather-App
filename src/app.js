d087669036a80a71c60d47d62f91c2a64a7671fa
GalaLozynska <gala.lozynska@icloud.com>
2020-08-10 23:03:15 +0200

    style search, deleted last updated

--- src/app.js
+++ src/app.js
@@ -49,7 +49,6 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt + 1000);
 }
 
 function displayForecast(response) {
